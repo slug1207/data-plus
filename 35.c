0dfb92aa36e86ac3fca5dd1832911e3c7ec1be74
slug1207 <106417561+slug1207@users.noreply.github.com>
2023-01-09 15:49:29 +0800

    Update 35.c

--- 35.c
+++ 35.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void sort(int array[],int size);
+void bubble(int array[],int n);
 int main()
 {
-    int s,in[3];
-    while(scanf("%d",&s)!=EOF){
-        while(s--){
+    int n,in[3];
+    while(scanf("%d",&n)!=EOF)
+    {
+        while(n--)
+        {
             int total=0;
-            for(int i=0;i<3;i++)
+            for(int i=0; i<3; i++)
                 scanf("%d",&in[i]);
             int pr[in[2]];
-            for(int i=0;i<in[2];i++)
+            for(int i=0; i<in[2]; i++)
                 scanf("%d",&pr[i]);
-            sort(pr,in[2]);
-            for(int i=0;i<in[1];i++)
+            bubble(pr,in[2]);
+            for(int i=0; i<in[1]; i++)
                 total+=pr[i];
             if(total<=in[0])
                 printf("%d\n",total);
@@ -22,19 +24,23 @@ int main()
                 printf("Impossible\n");
         }
     }
- }
-void sort(int array[],int size){
+}
+void bubble(int array[],int n)
+{
     {
-    int i, j, temp;
+        int i, j, tmp;
 
-    for (i = 0; i < size - 1; i++) {
-        for (j = 1; j < size; j++) {
-            if (array[j - 1] > array[j]) {
-                temp = array[j - 1];
-                array[j - 1] = array[j];
-                array[j] = temp;
+        for (i = 0; i < n; i++)
+        {
+            for (j = 1; j < n; j++)
+            {
+                if (array[j - 1] > array[j])
+                {
+                    tmp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = tmp;
+                }
             }
         }
     }
-}
 }
