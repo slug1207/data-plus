51941fd40364817c72a6242ad31eab6b622c37d5
slug1207 <106417561+slug1207@users.noreply.github.com>
2023-01-09 14:44:33 +0800

    Update 22.c

--- 22.c
+++ 22.c
@@ -1,26 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-int main(){
-    int in[9],a=0;
-    while(scanf("%d",&in[a])!=EOF & a<8)
+int main()
+{
+    int A[10],a=0,flag=0;
+    while(scanf("%d",&A[a])!=EOF & a<8)
         a++;
-    if((in[0]+in[1]+in[2])==3 || (in[0]+in[1]+in[2])==0)
+    if((A[0]+A[1]+A[2])==3 || (A[0]+A[1]+A[2])==0)
         printf("True\n");
-    else if((in[3]+in[4]+in[5])==3 || (in[3]+in[4]+in[5])==0)
+    else if((A[3]+A[4]+A[5])==3 || (A[3]+A[4]+A[5])==0)
         printf("True\n");
-    else if((in[6]+in[7]+in[8])==3 || (in[6]+in[7]+in[8])==0)
+    else if((A[6]+A[7]+A[8])==3 || (A[6]+A[7]+A[8])==0)
         printf("True\n");
-    else if((in[0]+in[3]+in[6])==3 || (in[0]+in[3]+in[6])==0)
+    else if((A[0]+A[3]+A[6])==3 || (A[0]+A[3]+A[6])==0)
         printf("True\n");
-    else if((in[1]+in[4]+in[7])==3 || (in[1]+in[4]+in[7])==0)
+    else if((A[1]+A[4]+A[7])==3 || (A[1]+A[4]+A[7])==0)
         printf("True\n");
-    else if((in[2]+in[5]+in[8])==3 || (in[2]+in[5]+in[8])==0)
+    else if((A[2]+A[5]+A[8])==3 || (A[2]+A[5]+A[8])==0)
         printf("True\n");
-    else if((in[0]+in[4]+in[8])==3 || (in[0]+in[4]+in[8])==0)
+    else if((A[0]+A[4]+A[8])==3 || (A[0]+A[4]+A[8])==0)
         printf("True\n");
-    else if((in[2]+in[4]+in[6])==3 || (in[2]+in[4]+in[6])==0)
+    else if((A[2]+A[4]+A[6])==3 || (A[2]+A[4]+A[6])==0)
         printf("True\n");
     else
         printf("False\n");
+
 }
