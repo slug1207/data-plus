ada3dbf96e5f7bfc02c7bb8a17376e5659c2b16a
slug1207 <106417561+slug1207@users.noreply.github.com>
2023-01-09 15:51:56 +0800

    Update 39.c

--- 39.c
+++ 39.c
@@ -6,22 +6,22 @@ int main()
     int n,in[3];
     scanf("%d",&n);
     while(n--){
-        int totol=0,x=0,x8=0;
+        int totol=0,a=0,b=0;
         for(int i=0;i<3;i++){
             scanf("%d",&in[i]);
             totol+=in[i];
             if(in[i]<60)
-                x++;
+                a++;
             if(in[i]>=80)
-                x8++;
+                b++;
         }
-        if(x==0)
+        if(a==0)
             printf("P\n");
-        else if((x==1) && (totol>=220))
+        else if((a==1) && (totol>=220))
             printf("P\n");
-        else if((x==1) && (totol<220))
+        else if((a==1) && (totol<220))
             printf("M\n");
-        else if((x==2) && (x8==1))
+        else if((a==2) && (b==1))
             printf("M\n");
         else
             printf("F\n");
