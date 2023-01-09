bcb6b66d23f8239df3098a787023a63bc4e816b5
slug1207 <106417561+slug1207@users.noreply.github.com>
2023-01-09 15:21:53 +0800

    Update 34.c

--- 34.c
+++ 34.c
@@ -6,9 +6,15 @@ int main()
     float a;
     int b;
     while(scanf("%f %d",&a,&b)!=EOF){
-        if(b==1)
-            printf("%.1f\n",((a-80)*0.7));
-        else
-            printf("%.1f\n",((a-70)*0.6));
+        if(b==1){
+            float ans =(a-80)*0.7;
+            printf("%.1f\n",ans);
+        }
+
+        else{
+                float ans=(a-70)*0.6;
+            printf("%.1f\n",ans);
+        }
+
     }
 }
